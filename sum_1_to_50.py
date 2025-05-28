9af3912fb6787e066258ef55ee4fa5c0684a9124
harsh-it-op <hs4641233@gmail.com>
2025-05-28 11:26:49 +0530

    Create sum_1_to_50.py

---
+++ sum_1_to_50.py
@@ -0,0 +1,11 @@
+# Task 2: Sum of Integers from 1 to 50
+
+# Initialize sum
+total = 0
+
+# Loop through numbers 1 to 50
+for num in range(1, 51):
+    total += num
+
+# Display result
+print(f"The sum of integers from 1 to 50 is: {total}")
